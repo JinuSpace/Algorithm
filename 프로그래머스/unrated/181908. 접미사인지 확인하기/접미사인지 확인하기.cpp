4090cb5ab914cb405f22b4dcad3c90d5234f6387
Jinu <97069553+JinuSpace@users.noreply.github.com>
2023-09-06 20:25:42 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string, string is_suffix) {
+    int answer = 0;
+    
+    vector<string> temp;
+    
+    for(int i =0;i<my_string.size();i++){
+        if(is_suffix== my_string.substr(i,my_string.size()+1)){
+            answer =1;
+        }
+        
+    }
+    
+    return answer;
+}
