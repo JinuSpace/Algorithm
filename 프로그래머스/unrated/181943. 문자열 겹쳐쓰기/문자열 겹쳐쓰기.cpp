83b7ead6561baae23fafae21ddf11c44fa796c96
Jinu <97069553+JinuSpace@users.noreply.github.com>
2023-09-06 20:23:56 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, string overwrite_string, int s) {
+    string answer = my_string;
+    
+    for (int i = 0; i < my_string.size(); i++) {
+        if (i < s) {
+            answer[i] = my_string[i];
+        } else if (i < s + overwrite_string.size()) {
+            answer[i] = overwrite_string[i - s];
+        } else {
+            answer[i] = my_string[i];
+        }
+    }
+    
+    return answer;
+}
