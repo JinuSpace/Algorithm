55d452b374435b43fe2dc3d5f0386a495a72e6d2
Jinu <97069553+JinuSpace@users.noreply.github.com>
2023-09-06 20:26:10 +0900

    [unrated] Title: 접미사 배열, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+vector<string> solution(string my_string) {
+    vector<string> answer;
+    
+    for(int i =0;i<my_string.size();i++){
+        answer.push_back(my_string.substr(i,my_string.size()+1));
+        
+    }
+    
+    sort(answer.begin(),answer.end());
+    
+    return answer;
+}
