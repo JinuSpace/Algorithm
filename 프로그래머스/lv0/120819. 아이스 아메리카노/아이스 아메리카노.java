7da5f25412bff82966c5e066970b4462721b14b9
Jinu <97069553+JinuSpace@users.noreply.github.com>
2023-09-17 22:08:51 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 71.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int money) {
+        int[] answer = {};
+        
+        answer = new int[2];
+        
+        int aa = 5500;
+        
+        answer[0] = money/aa ;
+        answer[1] = money%aa ;
+        
+        return answer;
+    }
+}
