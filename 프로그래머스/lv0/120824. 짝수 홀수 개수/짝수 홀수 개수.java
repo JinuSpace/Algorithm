a8787ef8c34ce25ae673325b9811bd50af87a116
Jinu <97069553+JinuSpace@users.noreply.github.com>
2023-09-17 22:10:15 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = {};
+        
+        answer = new int[2];
+        
+        int a = 0;
+        int b = 0;
+        
+        for(int i =0; i<num_list.length;i++){
+            if(num_list[i]%2 ==1){
+                b+=1;
+                
+            }else{
+                a+=1;
+            }
+            
+        }
+        
+        answer[0] = a;
+        answer[1] = b;
+        
+        return answer;
+    }
+}
