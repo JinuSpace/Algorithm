007ec272880da0f05ccbc33a6bc06eefaebe0b03
Jinu <97069553+JinuSpace@users.noreply.github.com>
2023-09-25 22:59:46 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    int odd = 0;
+    int even = 0;
+    
+    for(int i =0;i<num_list.size();i++){
+        if(i %2 == 0){
+            even += num_list[i];
+        }else{
+            odd += num_list[i];
+        }
+    }
+    
+    if(even>odd){
+        
+        return even;
+    }else if(even < odd){
+        
+        return odd; 
+    }else{
+        return even;
+    }
+    
+    return answer;
+}
