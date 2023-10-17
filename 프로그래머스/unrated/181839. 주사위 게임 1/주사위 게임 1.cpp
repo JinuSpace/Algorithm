544395f06316a45c890f1d0a58e2ee7da87e9729
Jinu <97069553+JinuSpace@users.noreply.github.com>
2023-10-17 22:58:12 +0900

    [level unrated] Title: 주사위 게임 1, Time: 0.01 ms, Memory: 4.07 MB -BaekjoonHub

--- 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.cpp
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.cpp
@@ -5,13 +5,12 @@ using namespace std;
 
 int solution(int a, int b) {
     int answer = 0;
-    
-    if (a % 2 == 1 && b % 2 == 1) {
-        answer = a * a + b * b;
-    } else if (a % 2 == 1 || b % 2 == 1) {
-        answer = 2 * (a + b);
-    } else {
-        answer = abs(a - b);
+    if(a %2 == 1 && a% 2 == 1){
+        answer = a*a+b*b;
+    }else if(a%2 ==0 && a%2 == 0){
+        answer= abs(a-b);
+    }else{
+        answer = 2* (a+b);
     }
     return answer;
 }
