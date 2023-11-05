7d92aac78f4d651f6cb235ae5c515cd1ab9714b1
Jinu <97069553+JinuSpace@users.noreply.github.com>
2023-11-05 23:03:33 +0900

    [level unrated] Title: 그림 확대, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181836. 그림 확대/그림 확대.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(vector<string> picture, int k) {
+    vector<string> answer;
+    
+    for(int i = 0; i < picture.size(); i++){
+        string str = "";
+        
+        for(int j = 0; j < picture[i].size(); j++){
+            str += string(k, picture[i][j]);
+        }
+        
+        for(int j = 0; j< k;j++){
+            answer.push_back(str);
+        }
+        
+    }
+    return answer;
+}
