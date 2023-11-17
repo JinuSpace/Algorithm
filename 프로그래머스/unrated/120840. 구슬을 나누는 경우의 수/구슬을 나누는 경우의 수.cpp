fbcfdf89e18f75e428913b929960a5d29154baf7
Jinu <97069553+JinuSpace@users.noreply.github.com>
2023-11-17 22:26:56 +0900

    [level unrated] Title: 구슬을 나누는 경우의 수, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/unrated/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.cpp
+++ 프로그래머스/unrated/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.cpp
@@ -3,14 +3,20 @@
 
 using namespace std;
 
-int solution(int balls, int share) {
-    long answer = 1L;
-    int cnt = 1;
-
-    while(cnt <= share) {
-        answer *= balls--;
-        answer /= cnt++;
+int fac(int n){
+    if(n == 1){
+        return 1;
     }
+    
+    return n * fac(n-1);     
+}
+
+
+int solution(int balls, int share) {
+    int answer = 0;
+    
+    answer = fac(balls) / (fac(balls - share) * fac(share));
+    
     
     return answer;
 }
