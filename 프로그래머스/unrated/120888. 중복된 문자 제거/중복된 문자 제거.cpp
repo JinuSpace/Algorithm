4037bae51e27f36889db6c0bee84268a2eafbe07
Jinu <97069553+JinuSpace@users.noreply.github.com>
2023-11-21 20:09:37 +0900

    [level unrated] Title: 중복된 문자 제거, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/unrated/120888. 중복된 문자 제거/중복된 문자 제거.cpp
+++ 프로그래머스/unrated/120888. 중복된 문자 제거/중복된 문자 제거.cpp
@@ -5,17 +5,5 @@ using namespace std;
 
 string solution(string my_string) {
     string answer = "";
-    vector<char> temp;
-    for(int i = 0;i<my_string.size();i++){
-        bool isExist = false;
-        for(char c : answer){
-            if (c == my_string[i]){
-                isExist = true;
-            }
-        }
-        if(!isExist){
-            answer+= my_string[i];
-        }
-    }
     return answer;
 }
