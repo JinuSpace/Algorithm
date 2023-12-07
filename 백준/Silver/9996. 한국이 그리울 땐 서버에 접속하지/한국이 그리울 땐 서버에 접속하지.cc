c754ff89d7cbd3be1463c6cfb3972b57687b6c67
Jinu <97069553+JinuSpace@users.noreply.github.com>
2023-12-07 23:53:23 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+ 
+int main()
+{
+	int N;
+	cin >> N;
+	
+	string ptn;
+	cin >> ptn;
+	
+	int pos = ptn.find('*');
+	string first = ptn.substr(0, pos);
+	string last = ptn.substr(pos+1, ptn.length());
+	
+	for(int i = 0; i < N; i++){
+		string input;
+		cin >> input;
+		
+		if(input.length() < first.length() + last.length()){
+			cout << "NE" << "\n";
+			continue;
+		}
+		
+		string compfirst = input.substr(0, first.length());
+		string complast = input.substr(input.length()-last.length(), input.length());
+		
+		if(compfirst == first && complast == last)
+			cout << "DA" << "\n";
+		else
+			cout << "NE" << "\n";
+	}	
+}
