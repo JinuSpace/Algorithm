a75b545796333d2a9bb64a20e50625df2df4581e
Jinu <97069553+JinuSpace@users.noreply.github.com>
2023-12-26 20:38:20 +0900

    [Silver IV] Title: 좋은 단어, Time: 4 ms, Memory: 2392 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, ret;
+string s;
+
+typedef long long ll;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> n;
+    for(int i = 0;i<n;i++){
+        cin >>s;
+        stack<char> stk;
+        
+        for(char c : s){
+            if(stk.size() && stk.top() == c){
+                stk.pop();
+            }else{
+                stk.push(c);
+            }
+            
+           
+        }
+        if(stk.size() == 0){
+            ret++;
+        }
+    }
+    
+    cout <<ret <<"\n";
+    
+    return 0;
+}
