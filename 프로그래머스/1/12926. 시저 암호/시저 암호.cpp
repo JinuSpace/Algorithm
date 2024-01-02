26beba08312e1fcee422f5497bf6ad877b13beaa
Jinu <97069553+JinuSpace@users.noreply.github.com>
2024-01-02 21:40:28 +0900

    [level 1] Title: 시저 암호, Time: 0.13 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s, int n) {
+    string answer = "";
+    
+    for(int i = 0;i<s.size();i++){
+        if(s[i] >=65 && s[i] <= 90){
+            if(s[i]+n >90){
+                answer+=s[i]+n-26;
+            }else{
+                answer+=s[i]+n;
+            }
+        }else if(s[i] >=97 && s[i] <= 122){
+            if(s[i]+n >122){
+                answer+=s[i]+n-26;
+            }else{
+                answer+=s[i]+n;
+            }
+        }else{
+            answer+=s[i];
+        }
+    }
+    return answer;
+}
