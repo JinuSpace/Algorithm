e5833294e2efa038df29f05f5d6ca059a9ae8c7c
Jinu <97069553+JinuSpace@users.noreply.github.com>
2024-01-19 00:07:37 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.49 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+int yaksoo(int n){
+    int count =1;
+    
+    for(int i = 1;i<=n/2;i++){
+        if(n%i == 0){
+            count++;
+        }
+    }
+    
+    return count;
+}
+
+int solution(int number, int limit, int power) {
+    int answer = 0;
+    
+    for(int i = 1;i<=number;i++){
+        int count = yaksoo(i);
+        if(count >limit){
+            count = power;
+        }
+        answer+= count;
+    }
+    
+    return answer;
+}
