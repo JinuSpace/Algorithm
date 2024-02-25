5654164111f7b24564ea23232a4c73e9f35d3161
Jinu <97069553+JinuSpace@users.noreply.github.com>
2024-02-25 21:29:21 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <stack>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+
+int dp[1001];
+
+void solve(int n) {
+    
+    
+    dp[1] = 1;
+    dp[2] = 3;
+
+    
+    for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2] * 2)%10007;
+    }
+    
+    
+    
+    
+    cout << dp[n] << "\n";
+    
+}
+
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
+    
+    cin >> n;
+    
+    
+    solve(n);
+    return 0;
+}
