580c9690753964ffcac7c4556b2d6a69297cb1b4
Jinu <97069553+JinuSpace@users.noreply.github.com>
2024-04-18 08:44:17 +0900

    [level 2] Title: ROOT 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
@@ -0,0 +1,12 @@
+-- 코드를 작성해주세요
+SELECT
+    ii.ITEM_ID
+    , ITEM_NAME
+FROM
+    ITEM_INFO ii
+    INNER JOIN ITEM_TREE it
+        ON ii.ITEM_ID = it.ITEM_ID
+WHERE
+    PARENT_ITEM_ID IS NULL
+ORDER BY
+    ITEM_ID
