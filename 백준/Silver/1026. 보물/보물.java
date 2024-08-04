1cb28457c93e7db9c5dc6fbb2d45dd0584bb9e42
Jinu <97069553+JinuSpace@users.noreply.github.com>
2024-08-04 19:25:44 +0900

    [Silver IV] Title: 보물, Time: 112 ms, Memory: 14404 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        Integer[] arr1 = new Integer[n];
+        Integer[] arr2 = new Integer[n];
+        int s = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr1[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr2[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2, Collections.reverseOrder());
+
+        for (int i = 0; i < n; i++) {
+            s += arr1[i] * arr2[i];
+        }
+
+        System.out.println(s);
+    }
+}
