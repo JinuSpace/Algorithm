5fbb8d7910e7d21beb47a4804c183f43a56ee50f
Jinu <97069553+JinuSpace@users.noreply.github.com>
2025-01-23 20:50:08 +0900

    [Silver III] Title: N과 M (4), Time: 144 ms, Memory: 17000 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] result;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        result = new int[M];
+
+        dfs(0);
+
+        System.out.println(sb);
+    }
+
+    static void dfs(int depth) {
+        if (depth == M) {
+            for (int num : result) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            result[depth] = i;
+            if( depth >0 ) {
+                if(result[depth -1] > result[depth]){
+                    continue;
+                }
+            }
+            dfs(depth + 1);
+        }
+    }
+}
