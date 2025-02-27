61e582f7363897aae626a293dd4825ae9614294e
Jinu <97069553+JinuSpace@users.noreply.github.com>
2025-02-27 22:56:37 +0900

    [Silver III] Title: 조합, Time: 108 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Silver/2407. 조합/조합.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static BigInteger combination(int n, int m) {
+        BigInteger result = BigInteger.ONE;
+        for (int i = 0; i < m; i++) {
+            result = result.multiply(BigInteger.valueOf(n - i))
+                    .divide(BigInteger.valueOf(i + 1));
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        System.out.println(combination(n, m));
+    }
+
+
+}
